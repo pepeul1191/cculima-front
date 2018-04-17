1cb82ce86130b1cd2b9aad47613b9929e23ff2b1
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-17 09:29:33 -0500

    Error del provider retorna al controlador

--- lib/App/Controller/Servicio.pm
+++ lib/App/Controller/Servicio.pm
@@ -2,12 +2,21 @@ package App::Controller::Servicio;
 use strict;
 use warnings;
 use Mojo::Base 'Mojolicious::Controller';
+use utf8;
+use JSON;
+use Encode qw(decode encode);
 use App::Provider::Servicio;
 #use Mojo::Log; use Data::Dumper;my $log = Mojo::Log->new;
-
 sub listar {
   my $self = shift;
-  $self->render(text => App::Provider::Servicio::listar());
+  my %mensaje = App::Provider::Servicio::listar();
+  if($mensaje{'codigo'} eq '200'){
+    my $rpta = %mensaje{'mensaje'};
+    $self->render(text =>  Encode::decode('utf8', $rpta), status => 200);
+  }else{
+    my $codigo = int(%mensaje{'codigo'});
+    $self->render(text => Encode::decode('utf8', JSON::to_json \%mensaje), status => $codigo);
+  }
 }
 
 1;
--- lib/App/Provider/Servicio.pm
+++ lib/App/Provider/Servicio.pm
@@ -3,39 +3,37 @@ use strict;
 use warnings;
 use REST::Client;
 use utf8;
-use JSON;
 use Encode qw(decode encode);
 use App::Config::Constants;
-#use Mojo::Log; use Data::Dumper;my $log = Mojo::Log->new;
-my $servicio_url = $App::Config::Constants::Data{'servicio_cculima'};
 
+my $servicio_url = $App::Config::Constants::Data{'servicio_cculima'};
+#use Mojo::Log; use Data::Dumper;my $log = Mojo::Log->new;
 sub listar {
-  my $rpta = '';
+  my %rpta = ();
   my $url = $servicio_url . 'servicio/listar';
   my $client = REST::Client->new();
   $client->GET($url);
   if( $client->responseCode() eq '200' ){
-    $rpta = decode('utf8', $client->responseContent());
+    $rpta{'tipo_mensaje'} = 'success';
+    $rpta{'codigo'} = 200;
+    $rpta{'mensaje'} = decode('utf8', $client->responseContent());
   } elsif( $client->responseCode() eq '500' ){
-    my %temp = ();
-    $temp{'tipo_mensaje'} = "error";
+    $rpta{'tipo_mensaje'} = 'error';
+    $rpta{'codigo'} = 500;
     my @temp = ('Se ha producido un error en el cliente REST', '' . $client->responseContent());
-    $temp{'mensaje'} = [@temp];
-    $rpta = Encode::decode('utf8', JSON::to_json \%temp);
+    $rpta{'mensaje'} = [@temp];
   } elsif( $client->responseCode() eq '404' ){
-    my %temp = ();
-    $temp{'tipo_mensaje'} = "error";
+    $rpta{'tipo_mensaje'} = 'error';
+    $rpta{'codigo'} = 404;
     my @temp = ('Recurso no encontrado en servicio', '' . $client->responseContent());
-    $temp{'mensaje'} = [@temp];
-    $rpta = Encode::decode('utf8', JSON::to_json \%temp);
+    $rpta{'mensaje'} = [@temp];
   }else{
-    my %temp = ();
-    $temp{'tipo_mensaje'} = "error";
+    $rpta{'tipo_mensaje'} = 'error';
+    $rpta{'codigo'} = 500;
     my @temp = ('Error: Excepción de servicio no capturada', '' . $client->responseContent());
-    $temp{'mensaje'} = [@temp];
-    $rpta = Encode::decode('utf8', JSON::to_json \%temp);
+    $rpta{'mensaje'} = [@temp];
   }
-  return $rpta;
+  return %rpta;
 }
 
 1;
